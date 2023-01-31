c395e82649ae424a80bb50d1d10337ef2369347b
Jaime Muinelo <jaimemuinelo@gmail.com>
2023-01-31 23:37:35 +0100

    .

--- Capítulo7/ejemplo.java
+++ Capítulo7/ejemplo.java
@@ -4,5 +4,6 @@ public class ejemplo{
     public static void main(String [] args){
         System.out.println("Ejemplo");
         System.out.println("Pequeño cambio por aquí");
+        System.out.println("Cambo en la nueva rama");
     }
 }
