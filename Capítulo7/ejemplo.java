8b2e86b74aca72b0fb1cb714640a28a701db8246
Jaime Muinelo <jaimemuinelo@gmail.com>
2023-01-31 23:10:46 +0100

    Pequenyo cambio

--- Capítulo7/ejemplo.java
+++ Capítulo7/ejemplo.java
@@ -3,5 +3,6 @@ package Capítulo7;
 public class ejemplo{
     public static void main(String [] args){
         System.out.println("Ejemplo");
+        System.out.println("Pequeño cambio por aquí");
     }
 }
